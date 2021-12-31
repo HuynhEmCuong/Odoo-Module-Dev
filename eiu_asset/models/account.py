c80deb3a030b71e38f97df65324bcd300980791d
HuynhEmCuong <huynhcuongem7597@gmail.com>
2021-12-31 14:41:49 +0700

    first commit

--- eiu_asset/models/account.py
+++ eiu_asset/models/account.py
@@ -6,3 +6,18 @@ class AccountMoveInherit(models.Model):
 
     account_file = fields.Binary(string="Upload chứng từ")
     account_file_name = fields.Char(string= "Tên file")
+
+    def open_qr_code(self):
+        print(self.id)
+        actions = {
+            'type': 'ir.actions.act_window',
+            'name': _('Creat Qr Code'),
+            'res_model' :'create.qrcode.wizard',
+            'target':'new',
+            'view_id': self.env.ref('eiu_asset.view_create_qrcode_wizard_form').id,
+            'view_mode':'form',
+            'context' :{
+                'default_account_move_id' : self.id
+            }
+        }
+        return actions
